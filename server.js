b7a8b4808b3144724cf12b2a6f83c764332f4d0b
Harish  Domale <99385144+HarishDomale@users.noreply.github.com>
2022-02-20 21:57:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ var express =require("express");  //lib-----Node Module
 var app=express();  // will create app object from express package
 
 var onAboutUs=function(req, res){
-    res.send("Chief Mentor :Ravi Tambade");
+    res.send("Harish Domale");
 };
 var onDefault=function(req, res){
-    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
+    res.send("<h1>Online Shopping</h1>"+
               "<hr/>"+
               "<ol>"+
-                    " <li>Mentoring for skill building</li>"+
-                    " <li>Workshops</li>"+
-                    " <li>Online Course</li>"+
-                    " <li>Corporate Training</li>"+
+                    " <li>Electronic</li>"+
+                    " <li>Home Appliance</li>"+
+                    " <li>Furniture</li>"+
+                    " <li>Fasshion</li>"+
              "</ol>");
 };
 app.get("/",onDefault);  // Request handler functions are registered
 app.get("/aboutus",onAboutUs);  // Request handler functions are registered
-var server=app.listen(8081);
-console.log("Server is running on port 8081");
+var server=app.listen(9090);
+console.log("Server is running on port 9090");
 // if external packages are required
 // we have to download those packages from internet using  package manager tool
 // Runtime--------tools-------------packages-----------------------config file
